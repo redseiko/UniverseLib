f5fdd674a4eb6141dbcd993bc137b03d6b47da37
redseiko <redseiko@users.noreply.github.com>
2024-02-04 11:12:22 -0800

    Change color values using names to their hex-code values.

--- src/Utility/ToStringUtility.cs
+++ src/Utility/ToStringUtility.cs
@@ -1,180 +1,174 @@
-﻿using System;
-using System.Collections;
+﻿namespace UniverseLib.Utility;
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Text;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UniverseLib.Runtime;
 
-namespace UniverseLib.Utility
+/// <summary>
+/// Provides utility for displaying an object's ToString result in a more user-friendly format.
+/// </summary>
+public static class ToStringUtility
 {
+    internal static Dictionary<string, MethodInfo> toStringMethods = new();
+
+    private const string nullString = "<color=#808080FF>null</color>";
+    private const string nullUnknown = nullString + " (?)";
+    private const string destroyedString = "<color=#FF0000FF>Destroyed</color>";
+    private const string untitledString = "<i><color=#808080FF>untitled</color></i>";
+
+    private const string eventSystemNamespace = "UnityEngine.EventSystem";
+
     /// <summary>
-    /// Provides utility for displaying an object's ToString result in a more user-friendly format.
+    /// Constrains the provided string to a maximum length, and maximum number of lines.
     /// </summary>
-    public static class ToStringUtility
+    public static string PruneString(string s, int chars = 200, int lines = 5)
     {
-        internal static Dictionary<string, MethodInfo> toStringMethods = new();
+        if (string.IsNullOrEmpty(s))
+            return s;
 
-        private const string nullString = "<color=grey>null</color>";
-        private const string nullUnknown = nullString + " (?)";
-        private const string destroyedString = "<color=red>Destroyed</color>";
-        private const string untitledString = "<i><color=grey>untitled</color></i>";
-
-        private const string eventSystemNamespace = "UnityEngine.EventSystem";
-
-        /// <summary>
-        /// Constrains the provided string to a maximum length, and maximum number of lines.
-        /// </summary>
-        public static string PruneString(string s, int chars = 200, int lines = 5)
+        StringBuilder sb = new(Math.Max(chars, s.Length));
+        int newlines = 0;
+        for (int i = 0; i < s.Length; i++)
         {
-            if (string.IsNullOrEmpty(s))
-                return s;
-
-            StringBuilder sb = new(Math.Max(chars, s.Length));
-            int newlines = 0;
-            for (int i = 0; i < s.Length; i++)
+            if (newlines >= lines || i >= chars)
             {
-                if (newlines >= lines || i >= chars)
-                {
-                    sb.Append("...");
-                    break;
-                }
-                char c = s[i];
-                if (c == '\r' || c == '\n')
-                    newlines++;
-                sb.Append(c);
+                sb.Append("...");
+                break;
             }
-
-            return sb.ToString();
+            char c = s[i];
+            if (c == '\r' || c == '\n')
+                newlines++;
+            sb.Append(c);
         }
 
-        /// <summary>
-        /// Returns the ToString result with a rich-text highlighted Type in trailing brackets. 
-        /// If the object does not implement ToString, then only the trailing highlighted Type will be returned.
-        /// </summary>
-        public static string ToStringWithType(object value, Type fallbackType, bool includeNamespace = true)
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// Returns the ToString result with a rich-text highlighted Type in trailing brackets. 
+    /// If the object does not implement ToString, then only the trailing highlighted Type will be returned.
+    /// </summary>
+    public static string ToStringWithType(object value, Type fallbackType, bool includeNamespace = true)
+    {
+        if (value.IsNullOrDestroyed() && fallbackType == null)
+            return nullUnknown;
+
+        Type type = value?.GetActualType() ?? fallbackType;
+
+        string richType = SignatureHighlighter.Parse(type, includeNamespace);
+
+        StringBuilder sb = new();
+
+        if (value.IsNullOrDestroyed())
         {
-            if (value.IsNullOrDestroyed() && fallbackType == null)
-                return nullUnknown;
-
-            Type type = value?.GetActualType() ?? fallbackType;
-
-            string richType = SignatureHighlighter.Parse(type, includeNamespace);
-
-            StringBuilder sb = new();
-
-            if (value.IsNullOrDestroyed())
+            if (value == null)
             {
-                if (value == null)
-                {
-                    sb.Append(nullString);
-                    AppendRichType(sb, richType);
-                    return sb.ToString();
-                }
-                else // destroyed unity object
-                {
-                    sb.Append(destroyedString);
-                    AppendRichType(sb, richType);
-                    return sb.ToString();
-                }
+                sb.Append(nullString);
+                AppendRichType(sb, richType);
+                return sb.ToString();
+            }
+            else // destroyed unity object
+            {
+                sb.Append(destroyedString);
+                AppendRichType(sb, richType);
+                return sb.ToString();
+            }
+        }
+
+        if (value is UnityEngine.Object obj)
+        {
+            if (string.IsNullOrEmpty(obj.name))
+                sb.Append(untitledString);
+            else
+            {
+                sb.Append('"');
+                sb.Append(PruneString(obj.name, 50, 1));
+                sb.Append('"');
             }
 
-            if (value is UnityEngine.Object obj)
+            AppendRichType(sb, richType);
+        }
+        else if (type.FullName.StartsWith(eventSystemNamespace))
+        {
+            // UnityEngine.EventSystem classes can have some obnoxious ToString results with rich text.
+            sb.Append(richType);
+        }
+        else
+        {
+            string toString = ToString(value);
+
+            if (type.IsGenericType
+                || toString == type.FullName
+                || toString == $"{type.FullName} {type.FullName}"
+                || toString == $"Il2Cpp{type.FullName}" || type.FullName == $"Il2Cpp{toString}")
             {
-                if (string.IsNullOrEmpty(obj.name))
-                    sb.Append(untitledString);
-                else
-                {
-                    sb.Append('"');
-                    sb.Append(PruneString(obj.name, 50, 1));
-                    sb.Append('"');
-                }
+                sb.Append(richType);
+            }
+            else // the ToString contains some actual implementation, use that value.
+            {
+                sb.Append(PruneString(toString, 200, 5));
 
                 AppendRichType(sb, richType);
             }
-            else if (type.FullName.StartsWith(eventSystemNamespace))
-            {
-                // UnityEngine.EventSystem classes can have some obnoxious ToString results with rich text.
-                sb.Append(richType);
-            }
-            else
-            {
-                string toString = ToString(value);
-
-                if (type.IsGenericType
-                    || toString == type.FullName
-                    || toString == $"{type.FullName} {type.FullName}"
-                    || toString == $"Il2Cpp{type.FullName}" || type.FullName == $"Il2Cpp{toString}")
-                {
-                    sb.Append(richType);
-                }
-                else // the ToString contains some actual implementation, use that value.
-                {
-                    sb.Append(PruneString(toString, 200, 5));
-
-                    AppendRichType(sb, richType);
-                }
-            }
-
-            return sb.ToString();
         }
 
-        private static void AppendRichType(StringBuilder sb, string richType)
+        return sb.ToString();
+    }
+
+    private static void AppendRichType(StringBuilder sb, string richType)
+    {
+        sb.Append(' ');
+        sb.Append('(');
+        sb.Append(richType);
+        sb.Append(')');
+    }
+
+    private static string ToString(object value)
+    {
+        if (value.IsNullOrDestroyed())
         {
-            sb.Append(' ');
-            sb.Append('(');
-            sb.Append(richType);
-            sb.Append(')');
+            if (value == null)
+                return nullString;
+            else // destroyed unity object
+                return destroyedString;
         }
 
-        private static string ToString(object value)
+        Type type = value.GetActualType();
+
+        // Find and cache the ToString method for this Type, if haven't already.
+
+        if (!toStringMethods.ContainsKey(type.AssemblyQualifiedName))
         {
-            if (value.IsNullOrDestroyed())
-            {
-                if (value == null)
-                    return nullString;
-                else // destroyed unity object
-                    return destroyedString;
-            }
+            MethodInfo toStringMethod = type.GetMethod("ToString", ArgumentUtility.EmptyTypes);
+            toStringMethods.Add(type.AssemblyQualifiedName, toStringMethod);
+        }
 
-            Type type = value.GetActualType();
+        // Invoke the ToString method on the object
 
-            // Find and cache the ToString method for this Type, if haven't already.
+        value = value.TryCast(type);
 
-            if (!toStringMethods.ContainsKey(type.AssemblyQualifiedName))
-            {
-                MethodInfo toStringMethod = type.GetMethod("ToString", ArgumentUtility.EmptyTypes);
-                toStringMethods.Add(type.AssemblyQualifiedName, toStringMethod);
-            }
+        string toString;
+        try
+        {
+            toString = (string)toStringMethods[type.AssemblyQualifiedName].Invoke(value, ArgumentUtility.EmptyArgs);
+        }
+        catch (Exception ex)
+        {
+            toString = ex.ReflectionExToString();
+        }
 
-            // Invoke the ToString method on the object
-
-            value = value.TryCast(type);
-
-            string toString;
-            try
-            {
-                toString = (string)toStringMethods[type.AssemblyQualifiedName].Invoke(value, ArgumentUtility.EmptyArgs);
-            }
-            catch (Exception ex)
-            {
-                toString = ex.ReflectionExToString();
-            }
-
-            toString = ReflectionUtility.ProcessTypeInString(type, toString);
+        toString = ReflectionUtility.ProcessTypeInString(type, toString);
 
 #if IL2CPP
-            if (value is Il2CppSystem.Type cppType)
-            {
-                Type monoType = Il2CppReflection.GetUnhollowedType(cppType);
-                if (monoType != null)
-                    toString = ReflectionUtility.ProcessTypeInString(monoType, toString);
-            }
+        if (value is Il2CppSystem.Type cppType)
+        {
+            Type monoType = Il2CppReflection.GetUnhollowedType(cppType);
+            if (monoType != null)
+                toString = ReflectionUtility.ProcessTypeInString(monoType, toString);
+        }
 #endif
 
-            return toString;
-        }
+        return toString;
     }
 }
